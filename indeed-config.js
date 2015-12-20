9f603a2faecf6e5f4791982cc175f509b5b092b9
Sam Kleiner <sam@skleiner.com>
2015-12-20 12:46:23 -0500

    Addid config file

---
+++ indeed-config.js
@@ -0,0 +1,7 @@
+// usage:
+// var PUBLISHER_ID = require('./indeed-config.js')
+// var Indeed = require('./indeed-callback.js')
+// var indeed_client = new Indeed(PUBLISHER_ID);
+
+// Change this to your publisher number
+module.exports = 'FILL_ME_IN';
